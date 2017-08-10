f4fbb514c2c1caf9b1282d1179fcc3bc4f265273
Adam Armstrong <adam.armstrong@amwinsauto.com>
2017-08-10 08:49:53 -0500

    Release 1.0

--- ti.androidfingerprintalertdialog/controllers/widget.js
+++ ti.androidfingerprintalertdialog/controllers/widget.js
@@ -1,13 +1,15 @@
 var usePasswordCallback, cancelCallback;
 
-exports.show = function(UsePasswordCB,CancelCB){
+$.fingerprintIcon.image = WPATH("android_fingerprint.png");
+
+function show(UsePasswordCB,CancelCB){
 	$.alertDialog.show();
 	usePasswordCallback = UsePasswordCB || {};
 	cancelCallback = CancelCB || {};
 };
 
-exports.success = function(){
-	$.fingerprintIcon.image = "/android_fingerprint_success.png";
+function success(){
+	$.fingerprintIcon.image = WPATH("android_fingerprint_success.png");
 	$.fingerprintLabel.color = "#009689";
 	$.fingerprintLabel.text = "Fingerprint recognized";
 	setTimeout(function() {
@@ -15,18 +17,20 @@ exports.success = function(){
 	}, 300);	
 };
 
-exports.failure = function(){
-	$.fingerprintIcon.image = "/android_fingerprint_failure.png";
+function failure(){
+	$.fingerprintIcon.image = WPATH("android_fingerprint_failure.png");
 	$.fingerprintLabel.color = "#f4511f";
 	$.fingerprintLabel.text = "Fingerprint not recognized.\nTry again.";
 };
 
 function fingerprintButtonPressed(e){
-	//Ti.API.info("Clicked: " + JSON.stringify(e));
 	if (e.index === 1) {			//"Use Password"
 		usePasswordCallback();
 	} else if (e.index === 0) {		//"Cancel"
 		cancelCallback();
 	}
 };
+exports.show = show;
+exports.success = success;
+exports.failure = failure;
 exports.fingerprintButtonPressed = fingerprintButtonPressed;
